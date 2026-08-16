b45914b005fd3c32e48a410dffa4c8ca77c42461
Annika Wickham <arwickha@ucsc.edu>
2026-08-16 13:41:04 -0700

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Annika Wickham on 8/16/26.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
