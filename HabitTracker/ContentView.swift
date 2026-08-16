29a579af5de590bfb6bf6a1c896421db6424eb6b
Annika Wickham <arwickha@ucsc.edu>
2026-08-16 14:03:44 -0700

    ?

--- HabitTracker/ContentView.swift
+++ HabitTracker/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("HIII, world!")
+            Text("bye, world!")
         }
         .padding()
     }
